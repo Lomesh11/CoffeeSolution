8037d5a45bf6b677a0fc3ff163ebba89abc649b7
lnandanw <lomesh.nandanwar@cognizant.com>
2021-09-12 12:47:18 +0530

    updated code

---
+++ Coffee.DataAccess.Test/DataAccessTest.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using NUnit;
+using NUnit.Framework;
+using Coffee.DataAccess;
+
+namespace Coffee.DataAccess.Test
+{
+    [TestFixture]
+    public class DataAccessTest
+    {
+        IDataAccess _dataAccess;
+        [SetUp]
+        public void Setup()
+        {
+            _dataAccess = new DataAccess();
+        }
+
+        [TestCase(3)] // Cappaccino 
+        [TestCase(2)] // coffee
+        [TestCase(1)] // latte
+        public void Test_isMilkAvailable(int qty)
+        {
+            bool result= _dataAccess.isMIlkAvailable(qty);
+            NUnit.Framework.Assert.AreEqual(true,result);
+        }
+
+
+        [TestCase(5)] // Cappaccino 
+        [TestCase(2)] // coffee
+        [TestCase(3)] // latte
+        public void Test_IsBeanAvailable(int qty)
+        {
+            bool result = _dataAccess.isMIlkAvailable(qty);
+            NUnit.Framework.Assert.AreEqual(true, result);
+        }
+    }
+}
---
+++ Coffee.DataAccess.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Coffee.DataAccess.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Coffee.DataAccess.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("69746845-725a-4615-bb1f-eb0790d135d5")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
